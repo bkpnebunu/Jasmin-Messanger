eaeab8c8183f1e49c861fed6d7bdc1ab76a73707
bkpnebunu <67269053+bkpnebunu@users.noreply.github.com>
2020-06-24 00:09:16 +0300

    fetch si post

--- Jasmin/Jasmin/Form1.cs
+++ Jasmin/Jasmin/Form1.cs
@@ -8,13 +8,60 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
 namespace Jasmin
 {
     public partial class Form1 : Form
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "kUkBSJNHvAhXv31pis29SDB11uD71edxr8x1TEwK",
+            BasePath = "https://jasmin-chat-demo.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private async void btnSend_ClickAsync(object sender, EventArgs e)
+        {
+            var messageData = new MessageData
+            {
+                Message = txtMesaj.Text,
+                Name = "current name",
+                Shoutout = false
+            };
+
+            SetResponse response = await client.SetAsync("Groups/GrupulMafiaHate/26", messageData);
+            MessageData result = response.ResultAs<MessageData>();
+
+            MessageBox.Show("trimite mesaju" + result);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            //if (client != null)
+            //{
+            //    MessageBox.Show("Merge");
+            //}
+        }
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.GetAsync("Groups/GrupulMafiaHate/");
+            MessageData responseMessage = response.ResultAs<MessageData>();
+            lstMesaje.Items.Add(responseMessage.Message);
+
         }
     }
 }
---
+++ Jasmin/Jasmin/MessageData.cs
@@ -0,0 +1,9 @@
+﻿namespace Jasmin
+{
+    internal class MessageData
+    {
+        public string Message { get; set; }
+        public string Name { get; set; }
+        public bool Shoutout { get; set; }
+    }
+}
