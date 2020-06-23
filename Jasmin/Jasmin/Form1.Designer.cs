03300ab1749189d28552306a9ebcea53b97e2f8a
Borkanie <43271944+Borkanie@users.noreply.github.com>
2020-06-24 00:26:25 +0300

    Ceacea

--- Jasmin/Jasmin/Form1.Designer.cs
+++ Jasmin/Jasmin/Form1.Designer.cs
@@ -28,37 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lstMesaje = new System.Windows.Forms.ListBox();
+            this.txtMesaj = new System.Windows.Forms.TextBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSend
             // 
-            this.button1.Location = new System.Drawing.Point(385, 391);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSend.Location = new System.Drawing.Point(385, 391);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 54);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "button1";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_ClickAsync);
             // 
-            // listBox1
+            // lstMesaje
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(162, 38);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(298, 329);
-            this.listBox1.TabIndex = 1;
+            this.lstMesaje.FormattingEnabled = true;
+            this.lstMesaje.Location = new System.Drawing.Point(162, 38);
+            this.lstMesaje.Name = "lstMesaje";
+            this.lstMesaje.Size = new System.Drawing.Size(298, 329);
+            this.lstMesaje.TabIndex = 1;
             // 
-            // textBox1
+            // txtMesaj
             // 
-            this.textBox1.Location = new System.Drawing.Point(162, 391);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 54);
-            this.textBox1.TabIndex = 2;
+            this.txtMesaj.Location = new System.Drawing.Point(162, 391);
+            this.txtMesaj.Multiline = true;
+            this.txtMesaj.Name = "txtMesaj";
+            this.txtMesaj.Size = new System.Drawing.Size(222, 54);
+            this.txtMesaj.TabIndex = 2;
             // 
             // listBox2
             // 
@@ -68,27 +69,29 @@
             this.listBox2.Size = new System.Drawing.Size(144, 433);
             this.listBox2.TabIndex = 3;
             // 
-            // button2
+            // btnGet
             // 
-            this.button2.Location = new System.Drawing.Point(385, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnGet.Location = new System.Drawing.Point(273, 8);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(52, 30);
+            this.btnGet.TabIndex = 4;
+            this.btnGet.Text = "button1";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.button1_ClickAsync);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 456);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnGet);
             this.Controls.Add(this.listBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtMesaj);
+            this.Controls.Add(this.lstMesaje);
+            this.Controls.Add(this.btnSend);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,11 +99,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lstMesaje;
+        private System.Windows.Forms.TextBox txtMesaj;
         private System.Windows.Forms.ListBox listBox2;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnGet;
     }
 }
 
